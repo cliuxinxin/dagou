7dc200a17b0acf255264368d421bd331b8f72d6a
cliuxinxin <cliuxinxin@gmail.com>
2017-03-09 22:38:16 +0800

    test auto deploy2

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Dagou Test
+                    Dagou Test 2
                 </div>
 
                 <div class="links">
