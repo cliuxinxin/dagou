8867128ef47383d954d5569affa63e4d51cfc768
cliuxinxin <cliuxinxin@gmail.com>
2017-03-11 20:04:38 +0800

    phpstorm test

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Dagou 11
+                    Dagou 12
                 </div>
 
                 <div class="links">
