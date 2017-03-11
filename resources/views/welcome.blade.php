8bb994d529fe4fcdb7da1e18bf43df8af38ef5c9
cliuxinxin <cliuxinxin@gmail.com>
2017-03-11 14:20:55 +0800

    hahah

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Dagou 9
+                    Dagou 10
                 </div>
 
                 <div class="links">
