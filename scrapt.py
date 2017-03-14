7dd57be9fd3783d0c1b3cf322ccc49504227b0a4
cliuxinxin <cliuxinxin@gmail.com>
2017-03-14 14:55:46 +0800

    scrape update

--- scrapt.py
+++ scrapt.py
@@ -8,6 +8,7 @@ import re
 
 
 #1st. Get html though domain
+
 def getBsObj(url):
 	try:
 		html = urlopen(url)
@@ -38,17 +39,22 @@ def getBsObjByWebdriver(url):
 		return None
 	return bsObj
 
+def getBsObjData(city):
+	if city['web'] == 1 :
+		return getBsObjByWebdriver(city['url'])
+	return getBsObj(city['url'])
+
 #2ed. Parse data base on the target info
 
 def parseDataTest(bsObj):
-	itemList = bsObj.find("ul", {"class":"qy_list"}).find_all("li")
+	itemList = bsObj.find("table",{"class":"myGVClass"}).find_all("tr",{"class":"myGVAltRow"})
 	records = []
 	for item in itemList:
 		record = {
-			"name":item.contents[2].text,
-			"url":item.contents[2].attrs['href'].replace("..", "http://www.cdggzy.com/app1"),
-			"city":'成都'+item.contents[1].text,
-			"start_date":item.contents[0].text,
+			"name":item.contents[2].text.replace("\n", ""),
+			"url":'http://caigou.my.gov.cn/ceinwz/'+item.contents[2].a.attrs["href"],
+			"city":'绵阳',
+			"start_date":item.contents[3].text.replace("\n", "").replace("[正在进行]", ""),
 		}
 		records.append(record)
 	print(records)
@@ -70,6 +76,36 @@ def parseData(bsObj):
 		records.append(record)
 	return records
 
+def parseMianyang(bsObj):
+	itemList = bsObj.find("table",{"class":"myGVClass"}).find_all("tr",{"class":"myGVAltRow"})
+	records = []
+	for item in itemList:
+		record = {
+			"name":item.contents[2].text.replace("\n", ""),
+			"url":'http://caigou.my.gov.cn/ceinwz/'+item.contents[2].a.attrs["href"],
+			"city":'绵阳',
+			"start_date":item.contents[3].text.replace("\n", "").replace("[正在进行]", ""),
+			"end_date":""
+		}
+		records.append(record)
+	return records
+
+def parseChengdu(bsObj):
+	itemList = bsObj.find("ul", {"class":"qy_list"}).find_all("li")
+	records = []
+	for item in itemList:
+		record = {
+			"name":item.contents[2].text,
+			"url":item.contents[2].attrs['href'].replace("..", "http://www.cdggzy.com/app1"),
+			"city":'成都'+item.contents[1].text,
+			"start_date":item.contents[0].text,
+			"end_date":""
+		}
+		records.append(record)
+	return records
+
+
+
 #3rd. Store data
 
 def storeData(records):
@@ -80,19 +116,44 @@ def storeData(records):
 
 	cur.executemany("INSERT IGNORE INTO items (name,url,city,start_date,end_date) VALUES (%(name)s,%(url)s,%(city)s,%(start_date)s,%(end_date)s)", records)
 	cur.connection.commit()
+	result = cur.rowcount
 
 	cur.close()
 	conn.close()
 
+	return result
+
 #4th. Repeat if neccsess
 
 
-bsObj = getBsObjByWebdriver("http://www.cdggzy.com/app1/two/jyxx_zbggmore.jsp")
+# bsObj = getBsObjByWebdriver("")
 
-if bsObj == None:
-	print("Title could not be found")
-else:
-	# parseDataTest(bsObj)
-	records = parseData(bsObj)
-	# print(records)
-	storeData(records)
+# bsObj = getBsObjByWebdriver("http://v2.dyggzy.com/?id=678")
+
+def scrape(city):
+	bsObj = getBsObjData(mianyang)
+
+	if bsObj == None:
+		print("Title could not be found")
+	else:
+		records = eval(city['method'] + '(bsObj)')
+		print(storeData(records))
+
+
+
+
+
+mianyang = {
+	'url':'http://caigou.my.gov.cn/ceinwz/WebInfo_List.aspx?newsid=2000&jsgc=&zfcg=0000000&tdjy=&cqjy=&PubDateSort=0&ShowPre=1&CbsZgys=0&zbfs=0&qxxx=0&showqxname=0&NewsShowPre=1&wsjj=0&showCgr=0&ShowOverDate=0&showdate=1&FromUrl=nourl',
+	'web': 0,
+	'method':'parseMianyang'
+}
+
+chengdu = {
+	'url':'http://www.cdggzy.com/app1/two/jyxx_zbggmore.jsp',
+	'web': 1,
+	'method':'parseChengdu'
+}
+
+scrape(mianyang)
+
