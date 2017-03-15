07306e2a9b3067567f4bb4885b787703092b1221
cliuxinxin <cliuxinxin@gmail.com>
2017-03-15 10:48:55 +0800

    hostpital op

--- app/Item.php
+++ app/Item.php
@@ -15,6 +15,6 @@ class Item extends Model
      */
     public function scopeHospital($query)
     {
-        return $query->where('name','LIKE','医院')->orWhere('name','LIKE','%设备%')->orWhere('name','LIKE','%竞争%')->orWhere('name','LIKE','%CT%');
+        return $query->where('name','LIKE','%医院%')->orWhere('name','LIKE','%设备%')->orWhere('name','LIKE','%竞争%')->orWhere('name','LIKE','%CT%');
     }
 }
